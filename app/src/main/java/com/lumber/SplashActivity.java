35617ec04308607bcef673dafd080888235d01e5
vinitkumar28 <vinitkumar28@gmail.com>
2017-12-28 11:43:03 +0530

    added log 2 brabch1

--- app/src/main/java/com/lumber/SplashActivity.java
+++ app/src/main/java/com/lumber/SplashActivity.java
@@ -2,16 +2,17 @@ package com.lumber;
 
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.view.View;
 import android.support.design.widget.NavigationView;
+import android.support.design.widget.Snackbar;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
 public class SplashActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
@@ -97,5 +98,9 @@ public class SplashActivity extends AppCompatActivity
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
+    }
+
+    public void method1(){
+        Log.i(SplashActivity.class.getSimpleName(), "First Merge");
     }
 }
---
+++ app/src/main/java/com/lumber/util/LumberUtil.java
@@ -0,0 +1,9 @@
+package com.lumber.util;
+
+/**
+ * Created by H193409 on 12/28/2017.
+ */
+
+public class LumberUtil {
+
+}
